d2b7bb31e4cb255e32cfe55939fb2fa26d141ac1
H.McLachlin <mclachlhu@students.dsbn.org>
2025-03-27 12:15:07 -0400

    hello

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+#H.mclachlin
+#March 27th, 2025
+#lists
+#program displays average of randome 0 - 100 numbers
+import random 
+numbers=[]
+for index in range(1,100):
+    num=random.randint(0,100)
+    print(num)
+    numbers.append(num)
+average=(sum(numbers)/100)
+print("the average of the random numbers is", average)
